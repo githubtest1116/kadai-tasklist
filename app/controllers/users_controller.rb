4ec3e8eecd227a5d3dec51a475a24cb67fcfa3e0
githubtest1116 <githubtest1116@gmail.com>
2017-11-25 22:48:11 +0000

    top page

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,24 @@
+class UsersController < ApplicationController
+  
+  #画面必要
+  def index
+  end
+
+  #画面必要
+  def show
+  end
+
+  #画面必要
+  def new
+  end
+  
+  def create
+  end
+
+  #画面必要
+  def edit
+  end
+  
+  def destroy
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,13 @@
 Rails.application.routes.draw do
-  root to: 'tasks#index'
+
+  root 'toppages#index'
+  #root to: 'tasks#index'
+
+  get 'signup' => 'users#new'
+
   resources :tasks
+  resources :users
+  resources :sessions
 
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
